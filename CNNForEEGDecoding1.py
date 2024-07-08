d32366cdcad27b2aa8a215ff9a6d805b5f59c498
Baris Baser <bbaser@andrew.cmu.edu>
2024-07-08 12:00:38 -0700

    Update CNNForEEGDecoding1.py

--- CNNForEEGDecoding1.py
+++ CNNForEEGDecoding1.py
@@ -1,8 +1,3 @@
-'''Configuring layers for signal processing and classification tasks using TensorFlow/Keras'''
-'''Deep Learning with Convolutinonal Neural Networks for EEG Decoding and Visualization'''
-'''https://onlinelibrary.wiley.com/doi/full/10.1002/hbm.23730'''
-'''This study applies CNNs to decode EEG signals for brain-computer interface applications with accuracy exceeding 90% in classifying cognitive states'''
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
@@ -19,3 +14,8 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
+
+'''Configuring layers for signal processing and classification tasks using TensorFlow/Keras'''
+'''Deep Learning with Convolutinonal Neural Networks for EEG Decoding and Visualization'''
+'''https://onlinelibrary.wiley.com/doi/full/10.1002/hbm.23730'''
+'''This study applies CNNs to decode EEG signals for brain-computer interface applications with accuracy exceeding 90% in classifying cognitive states'''
